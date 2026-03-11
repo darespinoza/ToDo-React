37121d54062a334d7b5da9380970af43d5c3d450
darespinoza_ubu24 <despinoza@uazuay.edu.ec>
2026-03-11 15:55:41 -0500

    [ADD] Exceptions handling on useLocalStorage custom hook

--- app/src/hooks/useLocalStorage.ts
+++ app/src/hooks/useLocalStorage.ts
@@ -2,17 +2,18 @@ import { useState, useEffect } from "react";
 
 export function useLocalStorage<T>(key: string, initialValue: T) {
   const [value, setValue] = useState<T>(() => {
-    const saved = localStorage.getItem(key);
-
-    if (saved) {
-      return JSON.parse(saved) as T;
+    try {
+      const saved = localStorage.getItem(key);
+      return saved ? (JSON.parse(saved) as T) : initialValue;
+    } catch {
+      return initialValue;
     }
-
-    return initialValue;
   });
 
   useEffect(() => {
-    localStorage.setItem(key, JSON.stringify(value));
+    try {
+      localStorage.setItem(key, JSON.stringify(value));
+    } catch {}
   }, [key, value]);
 
   // This is a tuple
